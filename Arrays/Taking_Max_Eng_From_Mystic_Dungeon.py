681434332392611aef9722381008c27296b5966a
Gaurav Pandey <officialgaurav04@gmail.com>
2025-10-10 07:06:57 +0530

    Taking maximum energy from mystic dungeon

---
+++ Arrays/Taking_Max_Eng_From_Mystic_Dungeon.py
@@ -0,0 +1,28 @@
+'''
+In a mystic dungeon, n magicians are standing in a line. Each magician has an attribute that gives you energy. Some magicians can give you negative energy, which means taking energy from you.
+
+You have been cursed in such a way that after absorbing energy from magician i, you will be instantly transported to magician (i + k). This process will be repeated until you reach the magician where (i + k) does not exist.
+
+In other words, you will choose a starting point and then teleport with k jumps until you reach the end of the magicians' sequence, absorbing all the energy during the journey.
+
+You are given an array energy and an integer k. Return the maximum possible energy you can gain.
+
+Note that when you are reach a magician, you must take energy from them, whether it is negative or positive energy.
+'''
+
+def maximumEnergy(energy, k):
+    n = len(energy)
+    dp = [0]*n
+
+    for i in range(n-1, -1, -1):
+        if i+k < n:
+            dp[i] = energy[i] + dp[i+k]
+        else:
+            dp[i] = energy[i]
+    
+    print(dp)
+    return max(dp)
+        
+
+ans = maximumEnergy([5,2,-10,-5,1], 3)
+print(ans)
