db4b4231b4f40592d3b18654682d7a8fdb9f89e6
Gaurav Pandey <officialgaurav04@gmail.com>
2025-11-23 20:59:04 +0530

    Four Sum

---
+++ Arrays/Four_Sum.py
@@ -0,0 +1,42 @@
+def fourSum(nums, target):
+    nums.sort()
+    n = len(nums)
+    res = []
+    for i in range(n-3):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        # pruning
+        if nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target:
+            break
+        if nums[i] + nums[n-1] + nums[n-2] + nums[n-3] < target:
+            continue
+
+        for j in range(i+1, n-2):
+            if j > i+1 and nums[j] == nums[j-1]:
+                continue
+            # pruning
+            if nums[i] + nums[j] + nums[j+1] + nums[j+2] > target:
+                break
+            if nums[i] + nums[j] + nums[n-1] + nums[n-2] < target:
+                continue
+
+            left, right = j+1, n-1
+            while left < right:
+                s = nums[i] + nums[j] + nums[left] + nums[right]
+                if s == target:
+                    res.append([nums[i], nums[j], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+                elif s < target:
+                    left += 1
+                else:
+                    right -= 1
+    return res
+
+nums = [1,0,-1,0,-2,2]
+target = 0
+print(fourSum(nums, target))
